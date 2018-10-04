86116a684b89dd452fba00b17fb6abb77af3dab0
ahmed mukul <37269657+amukul@users.noreply.github.com>
2018-10-04 23:01:25 +0600

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,6 @@
-// Future JavaScript will go here
+ let changeColor = document.getElementById('changeColor');
+
+  chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+  });
